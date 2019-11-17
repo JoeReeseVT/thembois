1a71cee3a72c3a3543b3900ac3536d15304f5f02
JoeReeseVT <joe@IdeeNoire.localdomain>
2019-11-17 14:36:56 -0500

    More cleaning idk how to use git

--- c/movingString.c
+++ c/movingString.c
@@ -1,17 +1,6 @@
-#ifndef LCD_H
-  #include "LCD.h"
-  #define LCD_H
-#endif
-
-#ifndef STDLIB_H
-  #include <stdio.h>
-  #define STDLIB_H
-#endif
-
-#ifndef STM32_H
-  #include "stm32l476xx.h"
-  #define STM32_H
-#endif
+#include "LCD.h"
+#include <stdio.h>
+#include "stm32l476xx.h"
 
 void delay(void) {
   unsigned long count;
@@ -21,13 +10,16 @@ void delay(void) {
 void movingString(uint8_t* str, uint8_t len) {
   unsigned int i, j;
   uint8_t buffer[6];
+
   for (i = 0; i < len; i++) {
+
     for (j = 0; j < 6; j++) {
       if (i + j < len)
         buffer[j] = *(str + i + j);
       else
         buffer[j] = *(str + i + j - len);
     }
+
     LCD_DisplayString(&buffer[0]);
     delay();
   }
