1cbbd3c97af43df29e5510b4c58769b9e5d03f60
JoeReeseVT <joe@IdeeNoire.localdomain>
2019-11-17 14:34:32 -0500

    Organized the repo a bit

---
+++ c/movingString.c
@@ -0,0 +1,34 @@
+#ifndef LCD_H
+  #include "LCD.h"
+  #define LCD_H
+#endif
+
+#ifndef STDLIB_H
+  #include <stdio.h>
+  #define STDLIB_H
+#endif
+
+#ifndef STM32_H
+  #include "stm32l476xx.h"
+  #define STM32_H
+#endif
+
+void delay(void) {
+  unsigned long count;
+  for(count = 0; count < 500000; count++) {} 
+}
+
+void movingString(uint8_t* str, uint8_t len) {
+  unsigned int i, j;
+  uint8_t buffer[6];
+  for (i = 0; i < len; i++) {
+    for (j = 0; j < 6; j++) {
+      if (i + j < len)
+        buffer[j] = *(str + i + j);
+      else
+        buffer[j] = *(str + i + j - len);
+    }
+    LCD_DisplayString(&buffer[0]);
+    delay();
+  }
+}
