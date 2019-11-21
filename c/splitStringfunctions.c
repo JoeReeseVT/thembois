ccb90d2a221319dcaa58e8f1329910d2e96f6fd2
jscott02 <57775288+jscott02@users.noreply.github.com>
2019-11-21 08:43:52 -0500

    Update splitStringfunctions.c

--- c/splitStringfunctions.c
+++ c/splitStringfunctions.c
@@ -1,17 +1,22 @@
+int CurrentIndexInString = 6; //global. Counts the index out of the whole string
+
 uint8_t* string_split(uint8_t* myStringPointer)
 {
 	return myStringPointer + 6;
 	
 }
 
-char get_char(uint8_t* newStringPointer, uint8_t* myStringPointer, int index, int stringLength, int currentLastIndex)
+//this will be for getting the character needed to be sent to the slave
+char get_char(uint8_t* newStringPointer, uint8_t* myStringPointer, int index, int stringLength)
 {
-	if((currentLastIndex + 1) <= stringLength)
+	
+	
+	if((currentIndexInString + 1) <= stringLength) //if we are below or at the last index
 	{
-		newStringPointer += index;
-		return (char)*newStringPointer;
+		currentIndexInString++;
+		return (char)*(newStringPointer + index);
 	}
-	else if((currentLastIndex + 1) == stringLength + 1)
+	else if((currentIndexInString + 1) == stringLength + 1)
 	{
 		return ' ';
 	}
